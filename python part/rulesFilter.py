071fcfcfa054de48ea6ffa0bc95181960fe52060
Yudong Cao <cydjohn@live.com>
2018-05-09 22:04:27 -0400

    update

--- python part/rulesFilter.py
+++ python part/rulesFilter.py
@@ -1,26 +1,44 @@
+# encoding:utf-8
 import pandas as pd
 from IPy import IP
-df = pd.read_csv("RuleSetA.csv")
 
+def judgeRuleSetA():
+    df = pd.read_csv("RuleSetA.csv")
+    result = []
+    for i in range(0, len(df)):
+        # 非单个IP地址情况
+        if len(df['srcIP'][i].split('-')) == 2 and len(df['destIP'][i].split('-')) == 2:
+            srcStartIP = IP(df['srcIP'][i].split('-')[0]).int()
+            srcEndIP = IP(df['srcIP'][i].split('-')[1]).int()
+            destStartIP = IP(df['destIP'][i].split('-')[0]).int()
+            destEndIP = IP(df['destIP'][i].split('-')[1]).int()
 
-for i in range(0, len(df)):
-    # 非单个IP地址情况
-    if len(df['srcIP'][i].split('-')) == 2 and len(df['destIP'][i].split('-')) == 2:
-        srcStartIP = IP(df['srcIP'][i].split('-')[0]).int()
-        srcEndIP = IP(df['srcIP'][i].split('-')[1]).int()
-        destStartIP = IP(df['destIP'][i].split('-')[0]).int()
-        destEndIP = IP(df['destIP'][i].split('-')[1]).int()
-
-        for j in range(i+1, len(df)):
-            if len(df['srcIP'][j].split('-')) == 2 and len(df['destIP'][j].split('-')) == 2:
-                checkSrcStartIP=IP(df['srcIP'][j].split('-')[0]).int()
-                checkSrcEndIP=IP(df['srcIP'][j].split('-')[1]).int()
-
-                checkDestStartIP=IP(df['destIP'][j].split('-')[0]).int()
-                checkDestEndIP=IP(df['destIP'][j].split('-')[1]).int()
-
-
+            for j in range(i+1, len(df)):
+                checkSrcStartIP = 0
+                checkSrcEndIP = 0
+                checkDestStartIP = 0
+                checkDestEndIP = 0
+                if len(df['srcIP'][j].split('-')) == 2 and len(df['destIP'][j].split('-')) == 2:
+                    checkSrcStartIP=IP(df['srcIP'][j].split('-')[0]).int()
+                    checkSrcEndIP=IP(df['srcIP'][j].split('-')[1]).int()
+                    checkDestStartIP=IP(df['destIP'][j].split('-')[0]).int()
+                    checkDestEndIP=IP(df['destIP'][j].split('-')[1]).int()
+                    # 单个点的情况
+                else:
+                    checkSrcStartIP=IP(df['srcIP'][j].split('-')[0]).int()
+                    checkSrcEndIP=IP(df['srcIP'][j].split('-')[0]).int()
+                    checkDestStartIP=IP(df['destIP'][j].split('-')[0]).int()
+                    checkDestEndIP=IP(df['destIP'][j].split('-')[0]).int()
                 if checkSrcStartIP > srcStartIP and checkSrcEndIP < srcEndIP:
                     # print(checkStartIP)
                     if checkDestStartIP > destStartIP and checkDestEndIP < destEndIP:
-                        print(destStartIP)
+                        if(df['action'][i] == df['action'][j]):
+                            result.append(df['srcIP'][j] + " and " + df['destIP'][j])
+    ipSet = set(result)
+    print(len(result))
+    print(len(ipSet))
+
+
+def judgeRuleSetBInA():
+    df1 = pd.read_csv("RuleSetA.csv")
+    df2 = pd.read_csv("RuleSetB.csv")
--- python part/rulesTransfer.py
+++ python part/rulesTransfer.py
@@ -7,6 +7,10 @@ from IPy import IP
 def translateToCSV(fileName):
 	rSrc = []
 	rDest = []
+	srcStart = []
+	srcEnd = []
+	destStart = []
+	destEnd = []
 	df = pd.read_csv(fileName+".txt")
 	for i in range(0,len(df)):
 		srcIP = df["srcIP"][i]
@@ -14,8 +18,28 @@ def translateToCSV(fileName):
 		rSrc.append(IP(srcIP,make_net=1).strNormal(3))
 		rDest.append(IP(destIP,make_net=1).strNormal(3))
 
+		srcTemp = IP(srcIP,make_net=1).strNormal(3).split('-')
+		if len(srcTemp) == 2:
+			srcStart.append(srcTemp[0])
+			srcEnd.append(srcTemp[1])
+		else:
+			srcStart.append(srcTemp[0])
+			srcEnd.append(srcTemp[0])
+
+		destTemp = IP(destIP,make_net=1).strNormal(3).split('-')
+		if len(destTemp) == 2:
+			destStart.append(destTemp[0])
+			destEnd.append(destTemp[1])
+		else:
+			destStart.append(destTemp[0])
+			destEnd.append(destTemp[0])
+
+
 	result = pd.DataFrame({"srcIP":rSrc,"destIP":rDest,"action":df["action"]})
+	result2 = pd.DataFrame({"srcIPStart":srcStart,"srcIPEnd":srcEnd,"destIPStart":destStart,"destIPEnd":destEnd,"action":df["action"]})
+	
 	result.to_csv(fileName+".csv")
+	result2.to_csv(fileName+"Plus.csv")
 
 
 translateToCSV("RuleSetA")
